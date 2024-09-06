3741f8114abe56ab2fbb5984272eeebcaa379792
Dmitrii Popov <MIntriy2033@yandex.ru>
2024-09-06 11:26:44 +0200

    k-means clustering with more colors

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import csv
 import random
 from sklearn.cluster import KMeans
+from matplotlib.colors import ListedColormap
 
 # Функция для записи случайных данных в CSV
 def writeCSV(filename, rows, cols):
@@ -29,7 +30,7 @@ writeCSV(filename, 100, 2)  # 100 строк, 2 столбца
 data = readCSV(filename)
 
 # Количество кластеров
-k = 6
+k = 5  # Пример с более чем 20 кластерами
 
 # Инициализация и обучение KMeans
 kmeans = KMeans(n_clusters=k, max_iter=100)
@@ -46,8 +47,15 @@ fig, axs = plt.subplots(1, 2, figsize=(14, 6))  # 1 строка, 2 столбц
 axs[0].scatter(data[:, 0], data[:, 1], s=30, color='gray')
 axs[0].set_title("Data Before K-Means Clustering")
 
+# Функция для генерации нужного количества цветов
+def generate_colors(num_colors):
+    cmap = plt.cm.get_cmap('tab20b', num_colors)  # Используем большую палитру
+    return cmap(np.linspace(0, 1, num_colors))
+
+# Генерация цветов для кластеров
+colors = generate_colors(k)
+
 # Подграфик 2: данные после кластеризации
-colors = ['r', 'g', 'b', 'c', 'm', 'y']
 for i in range(len(data)):
     axs[1].scatter(data[i][0], data[i][1], color=colors[assignments[i] % len(colors)], s=30)
 for i in range(len(centroids)):
